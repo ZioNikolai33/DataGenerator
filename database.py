bc898ce96499d5d687fcf6a8c283f2e762061791
Paolo Negro <paolo.negro00@gmail.com>
2025-09-09 21:49:55 +0200

    Add database class for MongoDb connectivity

---
+++ database.py
@@ -0,0 +1,38 @@
+import json
+from pymongo import MongoClient
+
+class database:
+    def __init__(self):
+        with open("config.json") as f:
+            config = json.load(f)
+
+        self.client = MongoClient(config["database"]["connectionString"])
+        self.db = self.client[config["database"]["databaseName"]]
+    
+    def getAllCollections(self):
+        return {
+            "AbilityScores": self.db["AbilityScores"].find(),
+            "Alignments": self.db["Alignments"].find(),
+            "Backgrounds": self.db["Backgrounds"].find(),
+            "Classes": self.db["Classes"].find(),
+            "Conditions": self.db["Conditions"].find(),
+            "DamageTypes": self.db["DamageTypes"].find(),
+            "Equipments": self.db["Equipments"].find(),
+            "EquipmentCategories": self.db["EquipmentCategories"].find(),
+            "Feats": self.db["Feats"].find(),
+            "Languages": self.db["Languages"].find(),
+            "Levels": self.db["Levels"].find(),
+            "MagicItems": self.db["MagicItems"].find(),
+            "MagicSchools": self.db["MagicSchools"].find(),
+            "Monsters": self.db["Monsters"].find(),
+            "Proficiencies": self.db["Proficiencies"].find(),
+            "Races": self.db["Races"].find(),
+            "RuleSections": self.db["RuleSections"].find(),
+            "Rules": self.db["Rules"].find(),
+            "Skills": self.db["Skills"].find(),
+            "Spells": self.db["Spells"].find(),
+            "Subclasses": self.db["Subclasses"].find(),
+            "Subraces": self.db["Subraces"].find(),
+            "Traits": self.db["Traits"].find(),
+            "WeaponProperties": self.db["WeaponProperties"].find(),
+        }
