43e900bb0f39236d2a584dbfc45e0686fa75184b
Paolo Negro <zionikolai33@hotmail.com>
2026-01-25 19:08:30 +0100

    UnitTests: Setup Project & Classes data

---
+++ TrainingDataGenerator.Tests/Unit/PartyMemberTests.cs
@@ -0,0 +1,14 @@
+namespace TrainingDataGenerator.Tests.Unit;
+
+public class PartyMemberTests
+{
+    #region Attribute Tests
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    #endregion
+}
